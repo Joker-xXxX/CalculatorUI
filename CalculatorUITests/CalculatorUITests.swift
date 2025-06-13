07dbbec4529ba8cbf1768e732061c20f9ba3898e
Kea-eve <xeoxeom1212@gmail.com>
2025-06-13 18:18:06 +0900

    계산기UI

---
+++ CalculatorUITests/CalculatorUITests.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorUITests.swift
+//  CalculatorUITests
+//
+//  Created by 이서범 on 6/11/25.
+//
+
+import Testing
+@testable import CalculatorUI
+
+struct CalculatorUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
